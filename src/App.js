91dda80024aac5f5a7b2ef8500ccb6540682af1c
MohsenSami <mohsensami88@gmail.com>
2023-08-20 22:29:31 +0330

    fetching data with useQuery

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
 import { Routes, Route, BrowserRouter, Link } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from 'react-query';
 import './App.css';
 import { Home } from './pages/Home';
 import { RQSuperHeroes } from './pages/RQSuperHeroes';
 import { SuperHeroes } from './pages/SuperHeroes';
 
+const queryClient = new QueryClient();
+
 function App() {
     return (
-        <BrowserRouter>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/super-heroes">Traditional Super Heroes</Link>
-                        </li>
-                        <li>
-                            <Link to="/rq-super-heroes">RQ Super Heroes</Link>
-                        </li>
-                    </ul>
-                </nav>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/super-heroes">Traditional Super Heroes</Link>
+                            </li>
+                            <li>
+                                <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+                            </li>
+                        </ul>
+                    </nav>
 
-                <Routes>
-                    <Route path="/super-heroes" element={<SuperHeroes />} />
-                    <Route path="/rq-super-heroes" element={<RQSuperHeroes />} />
-                    <Route path="/" element={<Home />} />
-                </Routes>
-            </div>
-        </BrowserRouter>
+                    <Routes>
+                        <Route path="/super-heroes" element={<SuperHeroes />} />
+                        <Route path="/rq-super-heroes" element={<RQSuperHeroes />} />
+                        <Route path="/" element={<Home />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </QueryClientProvider>
     );
 }
 
--- src/pages/RQSuperHeroes.jsx
+++ src/pages/RQSuperHeroes.jsx
@@ -1,7 +1,25 @@
+import axios from 'axios';
 import React from 'react';
+import { useQuery } from 'react-query';
+
+const fetchSuperHeros = () => {
+    return axios.get('http://localhost:4000/superheroes');
+};
 
 export const RQSuperHeroes = () => {
-    return <div>RQSuperheroes</div>;
+    const { data, isLoading } = useQuery('super-heroes', fetchSuperHeros);
+    if (isLoading) {
+        return <h2>Loading...</h2>;
+    }
+
+    return (
+        <>
+            <div>RQSuperheroes</div>
+            {data?.data.map((superhero) => {
+                return <div key={superhero.id}>{superhero.name}</div>;
+            })}
+        </>
+    );
 };
 
 export default RQSuperHeroes;
