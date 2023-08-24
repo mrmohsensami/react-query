cddc61e915002d3147dfd96ef8e50d01af755364
MohsenSami <mohsensami88@gmail.com>
2023-08-24 20:01:51 +0330

    Query Invalidation

--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -1,4 +1,4 @@
-import { useQuery, useMutation } from 'react-query';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
 import axios from 'axios';
 
 const fetchSuperHeros = () => {
@@ -28,5 +28,11 @@ export const useSuperHeroesData = (onSuccess, onError) => {
 };
 
 export const useAddSuperHeroData = () => {
-    return useMutation(addSuperHero);
+    const queryClient = useQueryClient();
+
+    return useMutation(addSuperHero, {
+        onSuccess: () => {
+            queryClient.invalidateQueries('super-heroes');
+        },
+    });
 };
